3f5e289d484b22626ce1c3e6f651d2d1d657802d
TheLastImperial <your-noreply-github-email>
2024-02-08 15:53:20 -0700

    Added register.

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirebase } from 'firebase/firestore/lite';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0MXIppQJ_bHU3q6T4mkxcXwuiF_CjVtM",
+  authDomain: "reactauth-4d6ed.firebaseapp.com",
+  projectId: "reactauth-4d6ed",
+  storageBucket: "reactauth-4d6ed.appspot.com",
+  messagingSenderId: "696262376705",
+  appId: "1:696262376705:web:42df1d5dc60a9f8d06ef00"
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB = getFirebase(FirebaseApp);
